754f79a0c58cb1ce99d0d5538105628622d89523
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2026-01-03 21:35:50 +0530

    feat : create hash key

---
+++ src/util/createKey.ts
@@ -0,0 +1,34 @@
+import config from "./config";
+import crypto from "crypto";
+import type { Request } from "express";
+import normalizeHeaders from "./normalizeHeaders.ts";
+import { normalizeURL } from "./normalizeURL.ts";
+
+const createKey = (req: Request): string => {
+  const normalizedUrl = normalizeURL(req.originalUrl);
+  const normalizedHeaders = normalizeHeaders(req);
+  const secret = config.secret;
+
+  const bodyHash =
+    req.body && Buffer.byteLength(req.body) > 0
+      ? crypto.createHmac("sha256", secret).update(req.body).digest("hex")
+      : "";
+
+  const reqObj = {
+    url: normalizedUrl,
+    method: req.method.toUpperCase(),
+    headers: normalizedHeaders,
+    bodyHash,
+  };
+
+  const reqBuffer = Buffer.from(JSON.stringify(reqObj));
+
+  const key = crypto
+    .createHmac("sha256", secret)
+    .update(reqBuffer)
+    .digest("hex");
+
+  return key;
+};
+
+export default createKey;
